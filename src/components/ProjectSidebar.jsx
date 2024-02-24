c47c6d9d4876be63b414fffac713fcaf9b1edbc3
hadeersalah <hadeersalah127@gmail.com>
2024-02-24 20:18:10 +0200

    adding sideNav

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectSidebar = () => {
+    return (
+        <aside>
+            <h2>Your Projects</h2>
+            <div>
+                <button>
+                    + Add a project
+                </button>
+            </div>
+            <ul>
+                
+            </ul>
+        </aside>
+    )
+}
+
+export default ProjectSidebar
