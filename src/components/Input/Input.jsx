f0e91f9b6ac9c76f9f89e7b07b31d6da197507ee
Hadeer Salah <hadeersalah127@gmail.com>
2024-02-26 09:55:12 +0200

    styling the input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Input = ({label, textarea, ...props}) =>{
+const Input = ({ label, textarea, ...props }) => {
+  const classes = "w-full p-1 rounded-sm border-b-2 bg-stone-200 border-stone-300 text-stone-600 focus:outline-none focus:border-stone -600"
   return (
-    <p>
-      <label>{label}</label>
-      {textarea ? <textarea {...props}/> : <input />}
+    <p className='flex flex-col gap-1 my-4'>
+      <label className='text-sm font-bold uppercase text-stone-500'>{label}</label>
+      {textarea ? <textarea {...props} className={classes} /> : <input className={ classes} />}
     </p>
   )
 }
--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -3,9 +3,14 @@ import Input from '../Input/Input'
 
 const NewProject = () => {
     return (
-        <div>
-            <menu>
-                <li></li>
+        <div className='w-[35rem] mt-16'>
+            <menu className='flex items-center justify-end gap-4 my-4'>
+                <li>
+                    <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
+                </li>
+                <li>
+                    <button className='bg-stone-800 text-stone-50 px-6 py-2 hover:bg-stone-950 rounded-md'>Save</button>
+                </li>
             </menu>
             <div>
                 <Input label="Title" />
