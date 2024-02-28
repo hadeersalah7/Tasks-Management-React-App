7f711195c0f55dc441c97b4959954067250a016a
Hadeer Salah <hadeersalah127@gmail.com>
2024-02-28 11:03:30 +0200

    adding the no new project component and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected/NoProjectSelected";
 import ProjectSidebar from "./components/Sidebar/ProjectSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
--- src/components/Sidebar/ProjectSidebar.jsx
+++ src/components/Sidebar/ProjectSidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 const ProjectSidebar = () => {
     return (
         <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 rounded-r-xl md:w-72'>
             <h2 className='mb-8 font-bold uppercase md:texl-xl text-stone-200'>Your Projects</h2>
             <div>
-                <button className='py-2 px-4 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>
-                    + Add a project
-                </button>
+                <Button>
+                    +  Add a project
+                </Button>
             </div>
             <ul>
                 
