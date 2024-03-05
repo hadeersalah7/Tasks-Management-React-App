0ff8f9386de153c11c84cdea0d78e27b5bbf005a
Hadeer Salah <hadeersalah127@gmail.com>
2024-03-05 08:27:32 +0200

    add the dialog modal

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -1,26 +1,35 @@
 import React, { useRef } from 'react'
 import Input from '../Input/Input'
+import Modal from '../Modal/Modal';
 
 const NewProject = ({ onAdd }) => {
     const title = useRef();
     const description = useRef();
     const dueDate = useRef()
-
+    const modal = useRef()
     const handleSave = () => {
         const enteredTitle = title.current.value;
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
+
+        if (enteredTitle.trim() == "" || enteredDescription.trim() == "" || enteredDueDate.trim == "") {
+            modal.current.open();
+            return;
+        } 
 
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
             dueDate: enteredDueDate
         })
+
     }
 
     return (
-        <div className='w-[35rem] mt-16'>
-            <menu className='flex items-center justify-end gap-4 my-4'>
+        <>
+            <Modal ref={modal} />
+            <div className='w-[35rem] mt-16'>
+                <menu className='flex items-center justify-end gap-4 my-4'>
                 <li>
                     <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
                 </li>
@@ -29,15 +38,16 @@ const NewProject = ({ onAdd }) => {
                         onClick={handleSave}
                     >Save</button>
                 </li>
-            </menu>
+                </menu>
             <div>
                 <Input type="text" ref={title} label="Title" />
 
                 <Input ref={description} label="Description" textarea />
 
                 <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
